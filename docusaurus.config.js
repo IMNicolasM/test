ea5c5aa908b0ba2d21aa5968186759b5cd13e4c2
Nicolas <nmontealegre@imaginacolombia.com>
2023-10-27 10:31:16 -0500

    Fixed: Link in doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/VueJs/tutorials/intro',
+                to: '/docs/VueJs/intro',
               },
             ],
           },
